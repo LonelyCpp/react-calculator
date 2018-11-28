20226990737ce16eedbba0c80f4be18276f6bed7
Ananthu Kanive <ananthu.kanive@ymedialabs.com>
2018-11-28 15:46:28 +0530

    added floating point numbers to test cases updated snapshot

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -9,6 +9,10 @@ test('infix to postfix', () => {
     expect(Calculator.infixToPostfix("9/9+9*9")).toEqual([9, 9, "/", 9, 9, "*", "+"]);
     expect(Calculator.infixToPostfix("9+9*9")).toEqual([9, 9, 9, "*", "+"]);
     expect(Calculator.infixToPostfix("9*9-9")).toEqual([9, 9, "*", 9, "-"]);    
+
+    expect(Calculator.infixToPostfix("9.4+9*9.1+9.001")).toEqual([9.4, 9, 9.1, "*", 9.001, "+", "+"]);
+    expect(Calculator.infixToPostfix("9/9.6+9*9.8")).toEqual([9, 9.6, "/", 9, 9.8, "*", "+"]);
+    expect(Calculator.infixToPostfix("9.0+9*9")).toEqual([9.0, 9, 9, "*", "+"]);
 });
 
 test('postfix evaluation', () => {
@@ -16,6 +20,12 @@ test('postfix evaluation', () => {
   expect(Calculator.evalPostfix([9, 1, '+'])).toBe(10);
   expect(Calculator.evalPostfix([3, 2, 5, '*', '+'])).toBe(13);
   expect(Calculator.evalPostfix([9, '+'])).toBe(NaN);
+
+  expect(Calculator.evalPostfix([9.1, 9.3, '+'])).toBe(18.4);
+  expect(Calculator.evalPostfix([9.01, 1, '+'])).toBe(10.01);
+  expect(Calculator.evalPostfix([3.3, 2.3, 5, '*', '+'])).toBe(14.8);
+  expect(Calculator.evalPostfix([9.6, '+'])).toBe(NaN);
+
 });
 
 test('renders correctly', () => {
